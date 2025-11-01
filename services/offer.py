6a388dfd7f76ded7ce090393eb2910c02b93d773
Jonathan Kashongwe <jokashongwe@gmail.com>
2025-11-01 17:20:16 +0100

    Add Optional

--- services/offer.py
+++ services/offer.py
@@ -1,16 +1,16 @@
-from db import bus_options, avion_options
+from db import bus_options, airplane_options
 
 def get_destinations(type: str="bus") -> list:
     """Retourne la liste unique des destinations"""
     if type == "bus":
         return bus_options.distinct("destination")
-    return avion_options.distinct("destination")
+    return airplane_options.distinct("destination")
 
 def get_departure_locations(type: str="bus") -> list:
     """Retourne la liste unique des villes de départ"""
     if type == "bus":
         return bus_options.distinct("departure_location")
-    return avion_options.distinct("departure_location")
+    return airplane_options.distinct("departure_location")
 
 
 def get_prices_with_company(departure, destination, type: str="bus", classe: str = None) -> list:
@@ -20,4 +20,4 @@ def get_prices_with_company(departure, destination, type: str="bus", classe: str
         return list(bus_options.find(query, projection))
     query = {"departure_location": departure, "destination": destination, "classe": classe}
     projection = {"_id": 0, "airline": 1, "price": 1}
-    return list(avion_options.find(query, projection))
+    return list(airplane_options.find(query, projection))
