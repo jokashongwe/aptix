c1e6202fd08720bd7b475dd7f392f57038bea22f
Jonathan Kashongwe <jokashongwe@gmail.com>
2025-11-04 15:53:37 +0100

    Fix values

--- routes/webhook.py
+++ routes/webhook.py
@@ -29,13 +29,13 @@ async def verify(request: Request):
 async def webhook(req: Request):
     data = await req.json()
     try:
-        print("Webhook reçu: ", data["entry"][0]["changes"][0]["value"])
         field_data  = data["entry"][0]["changes"][0]["value"]["messages"][0]
         
         phone, text = parse_data(field_data)
         if CHAT_OPTION == "TICKET":
             handle_message(phone, text)
         else: 
+            print(f"Field Data: {field_data}")
             await handle_fees_message(phone, text)
     except Exception as e:
         print("Erreur webhook: ", e)
